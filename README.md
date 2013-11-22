d2f5d81db256d309cf5c9e8ce4f6ecc8b37938a4
moznion <moznion@gmail.com>
2013-11-22 22:21:34 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::Croon - It's new $module
+
+# SYNOPSIS
+
+    use App::Croon;
+
+# DESCRIPTION
+
+App::Croon is ...
+
+# LICENSE
+
+Copyright (C) moznion.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+moznion <moznion@gmail.com>
---
+++ lib/App/Croon.pm
@@ -0,0 +1,39 @@
+package App::Croon;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Croon - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::Croon;
+
+=head1 DESCRIPTION
+
+App::Croon is ...
+
+=head1 LICENSE
+
+Copyright (C) moznion.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+moznion E<lt>moznion@gmail.comE<gt>
+
+=cut
+
